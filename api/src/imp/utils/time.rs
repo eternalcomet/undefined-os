7042bc0b2400603fc9b06474c615dcd65ae39c80
eternalcomet <mydragonos@outlook.com>
2025-04-08 15:53:22 +0800

    feat[time]: add sys_utimensat function stub

--- api/src/imp/utils/time.rs
+++ api/src/imp/utils/time.rs
@@ -1,9 +1,10 @@
+use crate::Kstat;
+use crate::ptr::{PtrWrapper, UserConstPtr, UserPtr};
 use arceos_posix_api::{self as api, ctypes::timeval};
 use axerrno::LinuxResult;
 use axhal::time::{monotonic_time_nanos, nanos_to_ticks};
+use core::ffi::c_char;
 use starry_core::{ctypes::Tms, task::time_stat_output};
-
-use crate::ptr::{PtrWrapper, UserPtr};
 
 pub fn sys_clock_gettime(clock_id: i32, tp: UserPtr<api::ctypes::timespec>) -> LinuxResult<isize> {
     unsafe { Ok(api::sys_clock_gettime(clock_id, tp.get()?) as _) }
@@ -24,4 +25,14 @@ pub fn sys_times(tms: UserPtr<Tms>) -> LinuxResult<isize> {
         }
     }
     Ok(nanos_to_ticks(monotonic_time_nanos()) as _)
+}
+
+pub fn sys_utimensat(
+    _dirfd: i32,
+    _path: UserConstPtr<c_char>,
+    _times: UserConstPtr<Kstat>,
+    _flags: i32,
+) -> LinuxResult<isize> {
+    warn!("[sys_utimensat] not implemented yet");
+    Ok(0)
 }
