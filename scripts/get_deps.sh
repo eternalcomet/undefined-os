908037c4f7af291499fcb6a3f6b90069fe7f3f36
eternalcomet <mydragonos@outlook.com>
2025-06-18 11:52:42 +0800

    build: update ArceOS repository link

--- scripts/get_deps.sh
+++ scripts/get_deps.sh
@@ -3,6 +3,6 @@
 AX_ROOT=.arceos
 
 test ! -d "$AX_ROOT" && echo "Cloning repositories ..." || true
-test ! -d "$AX_ROOT" && git clone https://github.com/eternalcomet/arceos "$AX_ROOT" --depth=1 || true
+test ! -d "$AX_ROOT" && git clone https://github.com/undefined-os/ArceOS "$AX_ROOT" || true
 
 $(dirname $0)/set_ax_root.sh $AX_ROOT
