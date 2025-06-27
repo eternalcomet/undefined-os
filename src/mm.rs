028fe297199950c7f446d760cc1775fa3b6e6164
eternalcomet <mydragonos@outlook.com>
2025-06-27 21:31:07 +0800

    feat[mm]: send SIGSEGV on page fault

--- src/mm.rs
+++ src/mm.rs
@@ -3,9 +3,12 @@ use axerrno::LinuxError;
 use axhal::mem::VirtAddr;
 use axhal::paging::MappingFlags;
 use axhal::trap::{PAGE_FAULT, register_trap_handler};
+use axsignal::{SignalInfo, Signo};
+use linux_raw_sys::general::SI_KERNEL;
+use starry_api::imp::task::signal::send_signal_process;
 use starry_api::imp::task::sys_exit_impl;
 use starry_core::mm::is_accessing_user_memory;
-use starry_core::task::current_process_data;
+use starry_core::task::{current_process, current_process_data};
 
 #[register_trap_handler(PAGE_FAULT)]
 fn handle_page_fault(vaddr: VirtAddr, access_flags: MappingFlags, is_user: bool) -> bool {
@@ -29,12 +32,19 @@ fn handle_page_fault(vaddr: VirtAddr, access_flags: MappingFlags, is_user: bool)
         .handle_page_fault(vaddr, access_flags)
     {
         warn!(
-            "{}: segmentation fault at {:#x}, exit!",
+            "{}: segmentation fault at {:#x}, send SIGSEGV.",
             axtask::current().id_name(),
             vaddr
         );
-        // TODO: correct exit code and signal
-        sys_exit_impl(LinuxError::EFAULT as _, false);
+        if send_signal_process(
+            current_process().get_pid(),
+            SignalInfo::new(Signo::SIGSEGV, SI_KERNEL as _),
+        )
+        .is_err()
+        {
+            error!("send SIGSEGV failed");
+            sys_exit_impl(LinuxError::EFAULT as _, false);
+        }
     }
     true
 }
